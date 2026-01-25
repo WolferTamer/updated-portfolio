3167f0b6b1c8a91499d42f1b1d1241e77ef6d4f7
ImWolfer <calebbcassin@gmail.com>
2026-01-25 13:47:13 -0600

    Revert "Fixed bottom being cut off on mobile"
    This reverts commit 322b4f984aa371c44bb2d36f80c93b9d8bd4c103.

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="page-wrapper">
       <div className="z-index-2 w-full pt-20 max-w-[32rem] md:max-w-[48rem] lg:max-w-[64rem]">
-        <div className="flex flex-row flex-wrap gap-10 justify-center items-stretch bottom-padding">
+        <div className="flex flex-row flex-wrap gap-10 justify-center items-stretch pb-5">
           <Profile />
           <Description />
         </div>
